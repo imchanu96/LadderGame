bc82394227c975016c975024032896ee78901a06
imchanu <cksdn9979@gmail.com>
2023-06-26 18:42:57 +0900

    사다리 게임에 Try-Catch문 추가

--- LadderGame/src/ladder/Main/LadderGameTest.java
+++ LadderGame/src/ladder/Main/LadderGameTest.java
@@ -17,19 +17,28 @@ public class LadderGameTest {
 
 		Message message = new Message();
 		message.startMessage(); 						// 게임 시작 메세지
-		numberOfUsers = scan.nextInt();
+		
+		try {
+			numberOfUsers = Integer.parseInt(scan.nextLine());
+		} catch (Exception e) {
+			numberOfUsers = 2;							// 문자열일시 2로 고정
+			System.out.println("숫자가 아닌 입력값으로 2로 고정합니다.");
+		}
+		
 		
 		Ladder ladder = new Ladder(numberOfUsers); 		// 플레이할 인원수 입력 받기
 		
 		message.selectWinPlayerMessage(); 				// 당첨 인원 고르라는 메세지
-		selectWinPlayer = scan.nextInt();
-		if (numberOfUsers > selectWinPlayer	
-				&& selectWinPlayer > 0) {				// 입력받은 인원수만큼 당첨 인원 저장
-			ladder.selectWinPlayer(selectWinPlayer); 	
-		}else {											// 잘못 입력하면 1로 고정
+		
+		
+		try {											// 입력받은 인원수만큼 당첨 인원 저장
+			selectWinPlayer = Integer.parseInt(scan.nextLine());
+			ladder.selectWinPlayer(selectWinPlayer); 
+		} catch (Exception e) {							// 잘못 입력하면 1로 고정
 			System.out.println("잘못 입력하셔서 당첨인원은 1로 됩니다.");
 			ladder.selectWinPlayer(1);
 		}
+		
 		
 		ladder.showLadder();
 
