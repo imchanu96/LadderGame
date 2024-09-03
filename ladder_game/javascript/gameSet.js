7282e996f8c59c1afb14cd393ca71310651926ee
imchanu96 <54897384+imchanu96@users.noreply.github.com>
2024-09-03 20:30:40 +0900

    게임 시작전 플레이어의 이름과 결과 정할수 있는 화면 구현중
    사다리가 표현되는 것은 그 후에 표현될수 있도록 조정중

--- ladder_game/javascript/gameSet.js
+++ ladder_game/javascript/gameSet.js
@@ -18,7 +18,7 @@ function headCountUpFnc() {
     }
 }
 
-function gameStartFnc(){ // 게임 시작
+function playerNameAndResultSelectFnc(){
     var titleBox = document.getElementById("titleBox");
     var headCount = parseInt(document.getElementById("headCountNum").value);
     var ladderSize = headCount * 2 - 1;
@@ -27,13 +27,30 @@ function gameStartFnc(){ // 게임 시작
         titleBox.removeChild(titleBox.firstChild);       
     }
 
+    var playerNameArea = document.createElement("table"); // 플레이어 이름 표현 구역
+    playerNameArea.id = "playerNameArea";
+    var playerNameTr = document.createElement("tr");
+    playerNameArea.appendChild(playerNameTr);
+
+    
+    for (let i = 0; i < headCount; i++) {
+        var playerNameTd = document.createElement("td");
+        var playerName = document.createElement("input");
+        playerName.className = "playerName";
+        playerName.type = "text";
+        playerName.placeholder = "이름";
+        playerNameTd.appendChild(playerName);
+        playerNameTr.appendChild(playerNameTd);
+        
+    }
+    titleBox.appendChild(playerNameArea);
+    
     var playerArr = new Array(10);
     for (let i = 0; i < playerArr.length; i++) {
         playerArr[i] = new Array(ladderSize).fill("");
         
     }
-
-   
+    
     for (let i = 0; i < playerArr.length; i++) { // 사다리 구성
         for (let j = 0; j < playerArr[0].length; j++) {
             if(j % 2 == 0){ //플레이어별 라인 (ㅣ)
@@ -44,6 +61,36 @@ function gameStartFnc(){ // 게임 시작
         }
         
     }
+    
+    var ladderArea = document.createElement("table"); // 사다리 초기 표현 구역
+    ladderArea.id = "ladderArea";
+    
+    
+    for (let i = 0; i < playerArr.length; i++) { // html화면에 구현
+        var ladderTr = document.createElement("tr");
+        for (let j = 0; j < playerArr[0].length; j++) {
+
+            var ladderTd = document.createElement("td");
+            ladderTr.appendChild(ladderTd);
+            ladderTd.innerHTML = ladderTd.innerHTML + playerArr[i][j];
+            
+        }
+        ladderArea.appendChild(ladderTr);
+    }
+    titleBox.appendChild(ladderArea);
+}
+
+
+function gameStartFnc(){ // 게임 시작
+    var titleBox = document.getElementById("titleBox");
+    var headCount = parseInt(document.getElementById("headCountNum").value);
+    var ladderSize = headCount * 2 - 1;
+    while ( titleBox.hasChildNodes() ) // 화면 지우기
+    {
+        titleBox.removeChild(titleBox.firstChild);       
+    }
+
+    
 
     var randNum = 0;
 
@@ -60,12 +107,5 @@ function gameStartFnc(){ // 게임 시작
     
     
 
-    for (let i = 0; i < playerArr.length; i++) { // html화면에 구현
-        for (let j = 0; j < playerArr[0].length; j++) {
-            
-            titleBox.innerHTML = titleBox.innerHTML + playerArr[i][j];
-            
-        }
-        titleBox.innerHTML = titleBox.innerHTML + '<br>';
-    }
+    
 }
